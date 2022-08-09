54e97a924625b7b0c3450ab2a623b503ced368f0
barry <mbrsyr@yahoo.com>
2022-08-09 19:50:35 +0200

    finish assignment Movies list

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -2,7 +2,7 @@
 movieList.style['background-color'] = 'red';
 movieList.style.display = 'block'; */
 
-const userChosenKeyName = 'user-chosen';
+/* const userChosenKeyName = 'user-chosen';
 const person = {
   'first-name': 'John',
   age: 30,
@@ -18,5 +18,65 @@ person.IsMarried = true;
 delete person.age;
 person.greet();
 console.log(person);
-console.log(person[1]);
+console.log(person[1]); */
 // if you use only numbers as keys for object then they orders wil be automatically sorted and that make you understand the array as a object
+
+const addMovieBtn = document.querySelector('#add-movie-btn');
+const searchBtn = document.querySelector('#search-btn');
+
+const movies = [];
+
+const renderMovies = (filter = '') => {
+  const movieList = document.querySelector('#movie-list');
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
+  const filteredMovies =
+    filter === ''
+      ? movies
+      : movies.filter((movie) => movie.info.title.includes(filter));
+
+  filteredMovies.forEach((movie) => {
+    const movieEl = document.createElement('li');
+    //movieEl.classList.add('list-item');
+    //movieEl.textContent = movie.info.title + ' __ ' + movie.info[extraName];
+    let text = movie.info.title + ' __ ';
+    for (const key in movie.info) {
+      if (key !== 'title') {
+        text += key + ' : ' + movie.info[key];
+      }
+    }
+    movieEl.textContent = text;
+    movieList.append(movieEl);
+  });
+};
+
+const addMovieHandler = () => {
+  const title = document.querySelector('#title').value.trim();
+  const extraName = document.querySelector('#extra-name').value.trim();
+  const extraValue = document.querySelector('#extra-value').value.trim();
+  if (title === '' || extraName === '' || extraValue === '') {
+    return;
+  }
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue,
+    },
+    id: Date.now().toString(),
+  };
+  movies.push(newMovie);
+  renderMovies();
+};
+
+const searchMovieHandler = () => {
+  const FilteredTitle = document.getElementById('filter-title').value.trim();
+  renderMovies(FilteredTitle);
+};
+
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
