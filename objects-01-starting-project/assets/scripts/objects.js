876c4baac6da601893b326da36bfd580e5fa967d
barry <mbrsyr@yahoo.com>
2022-08-09 14:08:46 +0200

    square bracket object

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -1,11 +1,16 @@
+/* const movieList = document.querySelector('#movie-list');
+movieList.style['background-color'] = 'red';
+movieList.style.display = 'block'; */
+
 const person = {
-  name: 'John',
+  'first-name': 'John',
   age: 30,
   hobbies: ['music', 'movies', 'sports'],
   greet() {
     console.log('Hi, I am ' + this.name);
   },
 };
+person['first-name'] = 'Mike';
 person.IsMarried = true;
 delete person.age;
 person.greet();
