43657b94639163fffc6f5b02b0136f145e959c92
barry <mbrsyr@yahoo.com>
2022-08-09 14:17:20 +0200

    array is an object

--- objects-01-starting-project/assets/scripts/objects.js
+++ objects-01-starting-project/assets/scripts/objects.js
@@ -9,9 +9,12 @@ const person = {
   greet() {
     console.log('Hi, I am ' + this.name);
   },
+  1: 'a',
 };
 person['first-name'] = 'Mike';
 person.IsMarried = true;
 delete person.age;
 person.greet();
 console.log(person);
+console.log(person[1]);
+// if you use only numbers as keys for object then they orders wil be automatically sorted and that make you understand the array as a object
