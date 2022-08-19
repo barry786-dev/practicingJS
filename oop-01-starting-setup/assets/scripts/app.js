662e6b15f05b9c9cdca64f776c07511226e193d4
barry <mbrsyr@yahoo.com>
2022-08-19 14:41:32 +0200

    super() order & this , ways to add methods

--- oop-01-starting-setup/assets/scripts/app.js
+++ oop-01-starting-setup/assets/scripts/app.js
@@ -1,18 +1,18 @@
-//const productList = {
-// products: [
-//   new Product(
-//     'A pillow',
-//     'https://via.placeholder.com/200x200',
-//     'A pillow is a type of soft bedding designed to support the head and upper body of a person.',
-//     10
-//   ),
-//   new Product(
-//     'A carpet',
-//     'https://via.placeholder.com/200x200',
-//     'A carpet is a type of floor covering made from a combination of thin woven fabric and batting or wadding.',
-//     20
-//   ),
-/* {
+/* const productList = {
+products: [
+  new Product(
+    'A pillow',
+    'https://via.placeholder.com/200x200',
+    'A pillow is a type of soft bedding designed to support the head and upper body of a person.',
+    10
+  ),
+  new Product(
+    'A carpet',
+    'https://via.placeholder.com/200x200',
+    'A carpet is a type of floor covering made from a combination of thin woven fabric and batting or wadding.',
+    20
+  ),
+{
       id: 1,
       title: 'Product 1',
       price: 10,
@@ -27,9 +27,9 @@
       description:
         'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.',
       image: 'https://via.placeholder.com/200x200',
-    }, */
-// ],
-/* render() {
+    },
+],
+render() {
     const renderHook = document.getElementById('app');
     const prodList = document.createElement('ul');
     prodList.className = 'product-list';
@@ -49,8 +49,8 @@
       prodList.append(prodEl);
     });
     renderHook.append(prodList);
-  }, */
-//};
+  },
+}; */
 const products = [
   {
     title: 'Product 1',
@@ -67,7 +67,6 @@ const products = [
     image: 'https://via.placeholder.com/200x200',
   },
 ];
-
 class Product {
   // title = 'DEFAULT';
   // imageUrl = 'assets/images/products/';
@@ -81,8 +80,36 @@ class Product {
     this.price = price;
   }
 }
+class ElementAttribute {
+  constructor(attrName, attrValue) {
+    this.name = attrName;
+    this.value = attrValue;
+  }
+}
+class Component {
+  constructor(renderHookId, shouldRender = true) {
+    this.hookId = renderHookId;
+    if (shouldRender) {
+      this.render();
+    }
+  }
+  render() {}
 
-class ShoppingCart {
+  CreateRootElement(tag, cssClasses, attributes) {
+    const rootElement = document.createElement(tag);
+    if (cssClasses) {
+      rootElement.className = cssClasses;
+    }
+    if (attributes && attributes.length > 0) {
+      for (const attr of attributes) {
+        rootElement.setAttribute(attr.name, attr.value);
+      }
+    }
+    document.getElementById(this.hookId).append(rootElement);
+    return rootElement;
+  }
+}
+class ShoppingCart extends Component {
   items = [];
 
   set cartItems(value) {
@@ -94,36 +121,49 @@ class ShoppingCart {
     const sum = this.items.reduce((a, b) => a + b.price, 0);
     return sum;
   }
-
+  constructor(renderHookId) {
+    super(renderHookId, false);
+    this.checkoutClicked = () => {
+      console.log('Checkout clicked');
+      console.log(this.items);
+    }
+    this.render();
+  }
   addProduct(product) {
     const updatedItems = [...this.items];
     updatedItems.push(product);
     this.cartItems = updatedItems;
   }
   render() {
-    const cartEl = document.createElement('section');
-    cartEl.className = 'cart';
-    const cartItems = `
+    // const cartEl = document.createElement('section');
+    // cartEl.className = 'cart';
+    const cartEl = this.CreateRootElement('section', 'cart', []);
+    cartEl.innerHTML = `
   <h2>Total: \$${0}</h2>
   <button>Checkout</button>
     `;
-    cartEl.innerHTML = cartItems;
+    const checkOutBtn = cartEl.querySelector('button');
+    //checkOutBtn.addEventListener('click', this.checkoutClicked.bind(this));
+    //checkOutBtn.addEventListener('click', () => this.checkoutClicked());
+    checkOutBtn.addEventListener('click', this.checkoutClicked);
     this.totalOutput = cartEl.querySelector('h2');
-    return cartEl;
+    //return cartEl;
   }
 }
-
-class ProductItem {
-  constructor(product) {
+class ProductItem extends Component {
+  constructor(product, renderHookId) {
+    super(renderHookId, false);
     this.product = product;
+    this.render();
   }
   addToCart() {
     console.log('Add to cart' + this.product.title);
     App.addProductToCart(this.product);
   }
   render() {
-    const prodEl = document.createElement('li');
-    prodEl.className = 'product-item';
+    //const prodEl = document.createElement('li');
+    //prodEl.className = 'product-item';
+    const prodEl = this.CreateRootElement('li', 'product-item', []);
     prodEl.innerHTML = `
       <div>
         <img src="${this.product.imageUrl}" alt="${this.product.title}">
@@ -136,61 +176,53 @@ class ProductItem {
       </div>`;
     const addCartButton = prodEl.querySelector('button');
     addCartButton.addEventListener('click', this.addToCart.bind(this));
-    return prodEl;
+    //return prodEl;
   }
 }
+class ProductList extends Component {
+  constructor(ArrayOfProducts, renderHookId) {
+    super(renderHookId, true);
+    this.fetchProducts(ArrayOfProducts);
+  }
 
-class ProductList {
-  /* products = [
-    new Product(
-      'A pillow',
-      'https://via.placeholder.com/200x200',
-      'A pillow is a type of soft bedding designed to support the head and upper body of a person.',
-      10
-    ),
-    new Product(
-      'A carpet',
-      'https://via.placeholder.com/200x200',
-      'A carpet is a type of floor covering made from a combination of thin woven fabric and batting or wadding.',
-      20
-    ),
-  ]; */
-  products = [];
-  constructor(ArrayOfProducts) {
+  fetchProducts(ArrayOfProducts) {
+    this.products = [];
     ArrayOfProducts.forEach((pro) => {
       const { title, image, description, price } = pro;
       this.products.push(new Product(title, image, description, price));
     });
+    this.renderProducts();
   }
-  render() {
-    const prodList = document.createElement('ul');
-    prodList.className = 'product-list';
+
+  renderProducts() {
     this.products.forEach((product) => {
-      const prodEl = new ProductItem(product).render();
-      prodList.append(prodEl);
+      new ProductItem(product, 'prod-list');
     });
-    return prodList;
   }
-}
 
-class Shop {
   render() {
-    const renderHook = document.getElementById('app');
-    this.cart = new ShoppingCart();
-    const cartEl = this.cart.render();
-    const productList = new ProductList(products);
-    const prodListEl = productList.render();
-
-    renderHook.append(cartEl);
-    renderHook.append(prodListEl);
+    const prodList = this.CreateRootElement('ul', 'product-list', [
+      new ElementAttribute('id', 'prod-list'),
+    ]);
+    if (this.products && this.products.length > 0) {
+      this.renderProducts();
+    }
   }
 }
-
+class Shop {
+  constructor() {
+    this.render();
+  }
+  render() {
+    this.cart = new ShoppingCart('app');
+    new ProductList(products, 'app');
+  }
+}
 class App {
   static cart;
   static init() {
     const shop = new Shop();
-    shop.render();
+    //shop.render();
     this.cart = shop.cart;
   }
   static addProductToCart(product) {
@@ -198,5 +230,4 @@ class App {
     // this.cart.render();
   }
 }
-
 App.init();
