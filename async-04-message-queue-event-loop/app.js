24060f3adad427f6e9e5659e33f895ab799c002b
barry <mbrsyr@yahoo.com>
2022-09-11 14:53:50 +0200

    promise chaining

--- async-04-message-queue-event-loop/app.js
+++ async-04-message-queue-event-loop/app.js
@@ -1,14 +1,14 @@
 const button = document.querySelector('button');
 const output = document.querySelector('p');
 
-function trackUserHandler() {
+/* function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
-    posData => {
+    (posData) => {
       setTimeout(() => {
         console.log(posData);
       }, 2000);
     },
-    error => {
+    (error) => {
       console.log(error);
     }
   );
@@ -16,10 +16,64 @@ function trackUserHandler() {
     console.log('Timer done!');
   }, 0);
   console.log('Getting position...');
-}
+} */
 
 button.addEventListener('click', trackUserHandler);
 
+const getPosition = (opts) => {
+  const promise = new Promise((resolve, reject) => {
+    navigator.geolocation.getCurrentPosition(
+      (success) => {
+        resolve(success);
+      },
+      (error) => {
+        reject(error);
+      },
+      opts
+    );
+  });
+  return promise;
+};
+
+const setTimer = (duration) => {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Done!');
+    }, duration);
+  });
+  return promise;
+};
+
+function trackUserHandler() {
+  /* getPosition().then((posData) => {
+    setTimer(2000).then((data) => {
+      console.log(posData, data);
+    });
+  }); */
+  let positionData;
+  getPosition()
+    .then((postData) => {
+      positionData = postData;
+      return setTimer(2000);
+    })
+    .then((data) => {
+      console.log(data, postData);
+    });
+  /* navigator.geolocation.getCurrentPosition(
+    (posData) => {
+      setTimer(2000).then((data) => {
+        console.log(data, posData);
+      });
+    },
+    (error) => {
+      console.log(error);
+    }
+  ); */
+  setTimer(0).then(() => {
+    console.log('Timer done!');
+  });
+  console.log('Getting position...');
+}
 // let result = 0;
 
 // for (let i = 0; i < 100000000; i++) {
