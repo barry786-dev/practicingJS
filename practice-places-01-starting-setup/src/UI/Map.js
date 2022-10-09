5d8dc4ac66c1a349be48346631ec65836f076914
barry <mbrsyr@yahoo.com>
2022-10-10 00:29:55 +0200

    OpenLayers

--- practice-places-01-starting-setup/src/UI/Map.js
+++ practice-places-01-starting-setup/src/UI/Map.js
@@ -4,7 +4,7 @@ export class Map {
   }
 
   render(coordinates) {
-    if (!google) {
+    /* if (!google) {
       alert('Could not load maps library - please try again later!');
       return;
     }
@@ -15,6 +15,19 @@ export class Map {
     new google.maps.Marker({
       position: coordinates,
       map: this.map,
+    }); */
+    document.getElementById('map').innerHTML = ''; // clear the <p> in the <div id="map">
+    const map = new ol.Map({
+      target: 'map',
+      layers: [
+        new ol.layer.Tile({
+          source: new ol.source.OSM(),
+        }),
+      ],
+      view: new ol.View({
+        center: ol.proj.fromLonLat([coordinates.lng, coordinates.lat]),
+        zoom: 16,
+      }),
     });
   }
 }
