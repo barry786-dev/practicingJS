10103144bbfb81efbdd111a608dd138a1b2a360a
barry <mbrsyr@yahoo.com>
2022-10-09 18:47:55 +0200

    getting DOM access inside a class

--- practice-places-01-starting-setup/src/SharePlace.js
+++ practice-places-01-starting-setup/src/SharePlace.js
@@ -1,2 +1,12 @@
-console.log('Hello World');
-console.log('I am a SharePlace');
+class PlaceFinder {
+  constructor() {
+    const addressForm = document.querySelector('form');
+    const locateUserBtn = document.getElementById('locate-btn');
+
+    locateUserBtn.addEventListener('click', this.locateUserHandler);
+    addressForm.addEventListener('submit', this.findAddressHandler);
+  }
+
+  locateUserHandler() {}
+  findAddressHandler() {}
+}
