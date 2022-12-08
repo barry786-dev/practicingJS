2ebf7f2b18f5d1a089ae48944db8cbc7ae79f011
barry <mbrsyr@yahoo.com>
2022-12-08 18:28:09 +0100

    more concepts in web components

--- getting-started-02-finished/webComp/tooltip.js
+++ getting-started-02-finished/webComp/tooltip.js
@@ -6,6 +6,7 @@ class Tooltip extends HTMLElement {
     this._tooltipIcon.textContent = `(?)${this.innerText}`; */
     this._tooltipContainer;
     this._tooltipText = '...No hint text found...';
+    this._tooltipVisible = false;
     this._styleObject = {
       position: 'relative',
     };
@@ -66,16 +67,56 @@ class Tooltip extends HTMLElement {
       this._hideTooltip.bind(this)
     );
     css(this, this._styleObject);
+    // this._render();
+  }
+  attributeChangedCallback(name, oldValue, newValue) {
+    if (oldValue === newValue) {
+      return;
+    }
+    if (name === 'message' && newValue !== '') {
+      this._tooltipText = newValue;
+    }
+  }
+  static get observedAttributes() {
+    return ['message'];
+  }
+
+  disconnectedCallback() {
+    this._tooltipIcon.removeEventListener(
+      'mouseenter',
+      this._showTooltip.bind(this)
+    ); // this is not doing any work actually
+    this._tooltipIcon.removeEventListener(
+      'mouseleave',
+      this._hideTooltip.bind(this)
+    );
+  }
+
+  _render() {
+    let tooltipContainer = this.shadowRoot.querySelector('div');
+    if (this._tooltipVisible) {
+      tooltipContainer = document.createElement('div');
+      tooltipContainer.textContent = this._tooltipText;
+      css(this._tooltipContainer, this._tooltipContainer_styleObject);
+      this.shadowRoot.appendChild(tooltipContainer);
+    } else {
+      if (tooltipContainer) {
+        this.shadowRoot.removeChild(tooltipContainer);
+      }
+    }
   }
   _showTooltip() {
     this._tooltipContainer = document.createElement('div');
     this._tooltipContainer.textContent = this._tooltipText;
     css(this._tooltipContainer, this._tooltipContainer_styleObject);
-
     this.shadowRoot.appendChild(this._tooltipContainer);
+    // this._tooltipVisible = true;
+    // this._render();
   }
   _hideTooltip() {
     this.shadowRoot.removeChild(this._tooltipContainer);
+    // this._tooltipVisible = false;
+    // this._render();
   }
 }
 
